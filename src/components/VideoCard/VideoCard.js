401d92f07a075ad79bd45c442fb2d3f52a46ec28
Gokul Gopi <gokulg1412@gmail.com>
2022-08-03 17:09:04 +0530

    fix: issues #1

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import '../VideoCard/VideoCard.css'
-import { BsThreeDotsVertical } from 'react-icons/bs'
-import { useVideo } from '../../context/VideoContext'
-import Option from './Option'
+import React from "react";
+import "../VideoCard/VideoCard.css";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useVideo } from "../../context/VideoContext";
+import Option from "./Option";
 
 const VideoCard = (props) => {
-    const { state, dispatch } = useVideo()
+  const { state, dispatch } = useVideo();
 
-    const handlerMoreOptions = (event) => {
-        event.preventDefault()
-        if (state.showOptions === props.id)
-            dispatch({ type: 'SET_SHOW_OPTIONS', payload: '' })
-        else
-            dispatch({ type: 'SET_SHOW_OPTIONS', payload: props.id })
+  const handlerMoreOptions = (event) => {
+    event.preventDefault();
+    if (state.showOptions === props.id)
+      dispatch({ type: "SET_SHOW_OPTIONS", payload: "" });
+    else dispatch({ type: "SET_SHOW_OPTIONS", payload: props.id });
+  };
 
-    }
-
-    return (
-        <div className='video-card'>
-            <div className='video-card-container'>
-                <div className='card-img'>
-                    <img src={`https://i.ytimg.com/vi/${props.videoID}/mqdefault.jpg`} alt="video-thumbnail" />
-                </div>
-                <div className="primary-details">
-                    <img src={props.channelImage} alt="channel" />
-                    <span className='name-of-video'>{props.name}</span>
-                    <button className='btn' onClick={(e) => handlerMoreOptions(e)} > <BsThreeDotsVertical /></button>
-                </div>
-
-                <div className="secondary-details">
-                    <span className='channel-name'>{props.channelName}</span>
-                    <span className='views'>{props.views} views</span>
-                </div>
-
-                {state.showOptions === props.id && <Option videoID={props.id} />}
-
-            </div>
+  return (
+    <div className="video-card">
+      <div className="video-card-container">
+        <div className="card-img">
+          <img
+            src={`https://i.ytimg.com/vi/${props.videoID}/mqdefault.jpg`}
+            alt="video-thumbnail"
+          />
         </div>
-    )
-}
+        <div className="primary-details">
+          <img src={props.channelImage} alt="channel" />
+          <span className="name-of-video">{props.name}</span>
+          <button className="btn" onClick={(e) => handlerMoreOptions(e)}>
+            <BsThreeDotsVertical />
+          </button>
+        </div>
 
-export default VideoCard
+        <div className="secondary-details">
+          <span className="channel-name">{props.channelName}</span>
+          <span className="views">{props.views} views</span>
+        </div>
+
+        {state.showOptions === props.id && <Option videoID={props.id} />}
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,49 +1,58 @@
-import React, { useEffect } from 'react'
-import '../Category/Category.css'
-import CategoryCard from '../../components/CategoryCard/CategoryCard'
-import { Link } from 'react-router-dom'
-import { networkCall } from '../../utils/networkCall'
-import { useVideo } from '../../context/VideoContext'
+import React, { useEffect } from "react";
+import "../Category/Category.css";
+import CategoryCard from "../../components/CategoryCard/CategoryCard";
+import { Link } from "react-router-dom";
+import { networkCall } from "../../utils/networkCall";
+import { useVideo } from "../../context/VideoContext";
 
 const Category = () => {
+  const { state, dispatch } = useVideo();
 
-    const { state, dispatch } = useVideo()
+  useEffect(() => {
+    const getCategories = async () => {
+      if (!state.category) {
+        const response = await networkCall("/category", "GET");
+        response.status === 200 &&
+          dispatch({
+            type: "SET_CATEGORIES",
+            payload: response.data.categories,
+          });
+      }
+    };
 
-    useEffect(() => {
+    getCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        const getCategories = async () => {
-            if (!state.category) {
-                const response = await networkCall('/category', "GET");
-                response.status === 200 && dispatch({ type: 'SET_CATEGORIES', payload: response.data.categories })
-            }
-        }
+  const setCategory = async (categoryID) => {
+    dispatch({ type: "SET_CURRENT_CATEGORY", payload: categoryID });
+    localStorage.setItem("Category", categoryID);
+  };
 
-        getCategories()
+  return (
+    <div className="category-page">
+      <p>Choose accordingly</p>
+      <div className="category-container">
+        {state.categories?.length < 1 ? (
+          <div className="category-loading">
+            <span>Loading categories...</span>
+          </div>
+        ) : (
+          state.categories.map((category) => {
+            return (
+              <Link
+                to="/videos"
+                key={category._id}
+                onClick={() => setCategory(category._id)}
+              >
+                <CategoryCard image={category.image} title={category.name} />
+              </Link>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+};
 
-    }, []);
-
-    const setCategory = async (categoryID) => {
-        dispatch({ type: 'SET_CURRENT_CATEGORY', payload: categoryID })
-        localStorage.setItem('Category', categoryID)
-    }
-
-
-    return (
-        <div className='category-page'>
-            <p>Choose accordingly</p>
-            <div className='category-container'>
-
-                {state.categories.map((category) => {
-                    return (
-                        <Link to='/videos' key={category._id} onClick={() => setCategory(category._id)
-                        }>
-                            <CategoryCard image={category.image} title={category.name} />
-                        </Link>)
-                })}
-
-            </div>
-        </div>
-    )
-}
-
-export default Category
+export default Category;
